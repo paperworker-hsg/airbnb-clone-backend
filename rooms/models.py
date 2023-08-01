a32058ec48acb305e4d1b7b369c7952c73cf2d12
paperworker.hsg@gmail.com <paperworker.hsg@gmail.com>
2023-08-01 20:30:35 +0900

    models ready.

--- rooms/models.py
+++ rooms/models.py
@@ -1,9 +1,8 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
 
-
-class Room(models.Model):
+class Room(CommonModel):
 
     """Room Model Definition"""
 
@@ -12,6 +11,7 @@ class Room(models.Model):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -28,13 +28,32 @@ class Room(models.Model):
         "users.User",
         on_delete=models.CASCADE,
     )
+    amenities = models.ManyToManyField(
+        "rooms.Amenity",
+    )
+    category = models.ForeignKey(
+        "categories.Category",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class Amenity(models.Model):
+class Amenity(CommonModel):
     """Amenity Definition"""
 
     name = models.CharField(max_length=150)
     description = models.CharField(
         max_length=150,
         null=True,
+        blank=True,
     )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
